7f65af9b3794c1ff83ac6b66786c3cddff77e53b
Ilia.Fedorov <fedilvy88@gmail.com>
2020-11-19 22:33:07 +0300

    задача для 8о раздела 4го урока 3го модуля

---
+++ module_3/lesson4_step8.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import time
+import os
+
+link = "http://suninjuly.github.io/file_input.html"
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_name("firstname")
+    input1.send_keys("Ivan")
+
+    input2 = browser.find_element_by_name("lastname")
+    input2.send_keys("Petrov")
+
+    input3 = browser.find_element_by_name("email")
+    input3.send_keys("some@mail.com")
+
+    input4 = browser.find_element_by_name("file")
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(current_dir, 'myfile.txt.txt')
+    input4.send_keys(file_path)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(5)
+    browser.quit()
+
+
+
