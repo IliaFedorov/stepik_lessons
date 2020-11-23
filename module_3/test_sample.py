541fde7f100c6b46dc2aa06bd40df376b1e0ed6b
Ilia.Fedorov <fedilvy88@gmail.com>
2020-11-24 02:45:37 +0300

    Правка ДЗ 3го модуля согласно code style и убрал unittest

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -1,34 +1,33 @@
 from selenium import webdriver
-import unittest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class TestBasket(unittest.TestCase):
-    def test_add_one_item_to_basket(self):
-        try:
-            # Arrange
-            link = "http://selenium1py.pythonanywhere.com/ru"
-            browser = webdriver.Chrome()
-            browser.implicitly_wait(5)
-            browser.get(link)
-            every_item_list_button = browser.find_element_by_link_text("Все товары")
-            every_item_list_button.click()
 
-            add_to_basket_button = WebDriverWait(browser, 5).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, ".col-lg-3 form"))
-            )
+def test_add_one_item_to_basket():
+    try:
+        # Arrange
+        link = "http://selenium1py.pythonanywhere.com/ru"
+        browser = webdriver.Chrome()
+        browser.implicitly_wait(5)
+        browser.get(link)
+        every_item_list_button = browser.find_element_by_link_text("Все товары")
+        every_item_list_button.click()
 
-            # Act
-            add_to_basket_button.click()
+        add_to_basket_button = WebDriverWait(browser, 5).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".col-lg-3 form"))
+        )
 
-            # Assert
-            first_product_name = browser.find_element_by_xpath("//h3/a").text
-            basket_add_notification = browser.find_element_by_css_selector(".alert-success .alertinner ").text
-            self.assertIn(first_product_name, basket_add_notification, "Product wasn't added to the basket")
+        # Act
+        add_to_basket_button.click()
 
-        finally:
-            browser.quit()
+        # Assert
+        first_product_name = browser.find_element_by_xpath("//h3/a").text
+        basket_add_notification = browser.find_element_by_css_selector(".alert-success .alertinner ").text
 
-if __name__ == "__main__":
-    unittest.main()
+        assert first_product_name in basket_add_notification, "Product wasn't added to the basket"
+
+    finally:
+        browser.quit()
+
+test_add_one_item_to_basket()
