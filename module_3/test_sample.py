020ebf214bd46094458a6a43e0c35b11edf5ccfa
Ilia.Fedorov <fedilvy88@gmail.com>
2020-11-24 02:53:42 +0300

    Правка ДЗ 3го модуля согласно code style и убрал unittest

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -5,24 +5,29 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_add_one_item_to_basket():
+    #Data
+    every_item_button_locator = "Все товары"
+    test_main_page_link = "http://selenium1py.pythonanywhere.com/ru"
+    basket_add_button_locator = ".col-lg-3 form"
+    first_product_name_locator = "//h3/a"
+    basket_add_notification_locator = ".alert-success .alertinner "
     try:
         # Arrange
-        link = "http://selenium1py.pythonanywhere.com/ru"
         browser = webdriver.Chrome()
         browser.implicitly_wait(5)
-        browser.get(link)
-        every_item_list_button = browser.find_element_by_link_text("Все товары")
+        browser.get(test_main_page_link)
+        every_item_list_button = browser.find_element_by_link_text(every_item_button_locator)
         every_item_list_button.click()
-        add_to_basket_button = WebDriverWait(browser, 5).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, ".col-lg-3 form"))
+        basket_add_button = WebDriverWait(browser, 5).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, basket_add_button_locator))
         )
 
         # Act
-        add_to_basket_button.click()
+        basket_add_button.click()
 
         # Assert
-        first_product_name = browser.find_element_by_xpath("//h3/a").text
-        basket_add_notification = browser.find_element_by_css_selector(".alert-success .alertinner ").text
+        first_product_name = browser.find_element_by_xpath(first_product_name_locator).text
+        basket_add_notification = browser.find_element_by_css_selector(basket_add_notification_locator).text
         assert first_product_name in basket_add_notification, "Product wasn't added to the basket"
 
     finally:
