52d0441293e24e366aeebb112ffb98117bac1738
Ilia.Fedorov <fedilvy88@gmail.com>
2020-12-05 17:49:38 +0300

    решения по 4му модулю

--- final/test_add_first_item_to_basket_pytest.py
+++ final/test_add_first_item_to_basket_pytest.py
@@ -4,11 +4,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
-test_main_page_link = "http://selenium1py.pythonanywhere.com/ru"
-every_item_button_locator = "Все товары"
-basket_add_button_locator = ".col-lg-3 form"
-first_product_name_locator = "//h3/a"
-basket_add_notification_locator = ".alert-success .alertinner "
+#test_main_page_link = "http://selenium1py.pythonanywhere.com/ru"
+#every_item_button_locator = "Все товары"
+#basket_add_button_locator = ".col-lg-3 form"
+#first_product_name_locator = "//h3/a"
+#basket_add_notification_locator = ".alert-success .alertinner "
 
 @pytest.fixture
 def browser():
@@ -19,6 +19,14 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
+@pytest.fixture(autouse=True, scope=module)
+def search_tag_data()
+    test_main_page_link = "http://selenium1py.pythonanywhere.com/ru"
+    every_item_button_locator = "Все товары"
+    basket_add_button_locator = ".col-lg-3 form"
+    first_product_name_locator = "//h3/a"
+    basket_add_notification_locator = ".alert-success .alertinner "
+
 @pytest.fixture(autouse=True)
 def prepare_data():
     print()
@@ -26,7 +34,7 @@ def prepare_data():
 
 
 
-def test_add_one_item_to_basket_with_notification(browser,prepare_data):
+def test_add_one_item_to_basket_with_notification(browser,search_tag_data):
     #Data
     assert_error_message = "Either product wasn't added to the basket or there was no notification about it"
 
---
+++ module_4/lesson6_step3.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pytest
+import time
+import math
+
+#data
+answer = math.log(int(time.time()))
+paste_result_locator = "#ember89"
+submit_button_locator = ".submit-submission"
+
+#arrange
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+#act   #, "896", "897", "898", "899","903","904","905"])
+@pytest.mark.parametrize('site_link', ["895"])
+def test_get_correct_answer(browser, site_link):
+    link = f"https://stepik.org/lesson/236{site_link}/step/1"
+    browser.get(link)
+    paste_result = browser.find_element_by_css_selector(paste_result_locator)
+    paste_result.send_keys(answer)
+    submit_button = browser.find_element_by_css_selector(submit_button_locator)
+    submit_button.click
+    time.sleep(10)
