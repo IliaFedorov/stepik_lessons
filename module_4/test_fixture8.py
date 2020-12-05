669709b9363835d0c77fc29a2f8d91cddc918391
Ilia.Fedorov <fedilvy88@gmail.com>
2020-12-05 15:16:02 +0300

    решения по 4му модулю

--- module_4/test_fixture8.py
+++ module_4/test_fixture8.py
@@ -15,12 +15,12 @@ def browser():
 
 
 @pytest.mark.smoke
-def test_guest_should_see_login_link(self, browser):
+def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
 
 
 @pytest.mark.regression
-def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+def test_guest_should_see_basket_link_on_the_main_page(browser):
     browser.get(link)
     browser.find_element_by_css_selector(".basket-mini .btn-group > a")
