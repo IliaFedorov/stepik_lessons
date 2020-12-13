b1b5b5639890a2df72c3f2da353d53ee7d23ca7b
Ilia.Fedorov <fedilvy88@gmail.com>
2020-12-14 01:22:47 +0300

    работа по 5му модулю 4ый пункт Добавил метод is_element_present

--- module_5/conftest.py
+++ module_5/conftest.py
@@ -2,30 +2,15 @@ import pytest
 from selenium import webdriver
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en-gb",
+    parser.addoption('--language', action='store', default="en-GB",
                      help="Choose language: ru, en-gb, es, fr")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser = webdriver.Chrome()
-    language = request.config.getoption("language")
-    link_language = None
-
-    if language == "ru":
-
-        print("\nopening russian language web page..")
-        link_language = language
-
-    elif language == "es":
-        print("\nopening spanish language web page..")
-        link_language = language
-
-    elif language == "fr":
-        print("\nopening spanish language web page..")
-        link_language = language
-    else:
-        print("\nno language parameter was given, opening english language web page..")
-        link_language = language
+    request.config.getoption('language')
+    esoptions = webdriver.ChromeOptions()
+    esoptions.add_argument('language')
 
     yield browser
     print("\nquit browser..")
