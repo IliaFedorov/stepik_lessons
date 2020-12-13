76acdb4dbc75a9759e02ea04bf688dcbbd052bb5
Ilia.Fedorov <fedilvy88@gmail.com>
2020-12-13 13:29:14 +0300

    работа по 5му модулю 4ый пункт Добавил метод is_element_present

--- module_5/pages/main_page.py
+++ module_5/pages/main_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        link = self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid")
+        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         link.click()
 
     def should_be_login_link(self):
