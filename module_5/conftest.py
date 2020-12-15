470d906bfd8cbc8b5b58979f397f5c8f8ab002ad
Ilia.Fedorov <fedilvy88@gmail.com>
2020-12-15 17:32:09 +0300

    работа по 5му модулю

--- module_5/conftest.py
+++ module_5/conftest.py
@@ -3,19 +3,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en-GB",
+    parser.addoption('--language', action='store', default="en-gb",
                      help="Choose language: ru, en-gb, es, fr")
 
 @pytest.fixture(scope="function")
 def browser(request):
 
     language = request.config.getoption('language')
-    options = Options()
+
+    options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
 
     print("\nopening browser..")
 
     browser = webdriver.Chrome(options=options)
+    browser.maximize_window()
+    browser.implicitly_wait(10)
 
     yield browser
     print("\nquit browser..")
