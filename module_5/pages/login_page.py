71c7bc474b93ca10d4375f262f68ef5d668f14c1
Ilia.Fedorov <fedilvy88@gmail.com>
2020-12-17 21:44:09 +0300

    добавление файлов по 5.5.13

--- module_5/pages/login_page.py
+++ module_5/pages/login_page.py
@@ -25,5 +25,5 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), register_form_fail_notification
 
-    def register_new_user(self, username, password):
+    #def register_new_user(self, username, password):
         #email = str(time.time()) + "@fakemail.org"
